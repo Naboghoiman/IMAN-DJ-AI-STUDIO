727d333900cb8d19a1e8c544b7dc3bbf0d774293
Naboghoiman <156505016+Naboghoiman@users.noreply.github.com>
2026-08-05 20:57:56 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
-// IMAN DJ AI STUDIO ENGINE
+// IMAN DJ AI STUDIO CONTROL ENGINE
+
 
 let decks = {
+
 A:{
 audio:null,
-playing:false,
-volume:1
+volume:0.8
 },
+
 B:{
 audio:null,
-playing:false,
-volume:1
+volume:0.8
 }
+
 };
 
+
+
+// LOAD MUSIC
 
 function loadTrack(deck,input){
 
@@ -20,91 +25,129 @@ let file=input.files[0];
 
 if(!file)return;
 
-decks[deck].audio=new Audio(URL.createObjectURL(file));
+
+decks[deck].audio = new Audio(
+URL.createObjectURL(file)
+);
+
 
 document.getElementById(
 "track"+deck
 ).innerHTML=file.name;
 
+
 }
 
+
+
+
+// PLAY
 
 function playTrack(deck){
 
-let d=decks[deck];
+let player=decks[deck].audio;
 
-if(d.audio){
+if(player){
 
-d.audio.volume=d.volume;
+player.volume=decks[deck].volume;
 
-d.audio.play();
-
-d.playing=true;
+player.play();
 
 }
 
 }
 
+
+
+// PAUSE
 
 function pauseTrack(deck){
 
-let d=decks[deck];
+let player=decks[deck].audio;
 
-if(d.audio){
+if(player){
 
-d.audio.pause();
-
-d.playing=false;
+player.pause();
 
 }
 
 }
 
+
+
+// STOP
 
 function stopTrack(deck){
 
-let d=decks[deck];
+let player=decks[deck].audio;
 
-if(d.audio){
+if(player){
 
-d.audio.pause();
+player.pause();
 
-d.audio.currentTime=0;
-
-d.playing=false;
+player.currentTime=0;
 
 }
 
 }
 
+
+
+
+// VOLUME
 
 function setVolume(deck,value){
 
 decks[deck].volume=value/100;
 
-if(decks[deck].audio){
-
-decks[deck].audio.volume=value/100;
-
-}
-
 }
 
 
 
-function effectOn(name){
-
-alert(name+" activated");
-
-}
-
-
+// SYNC
 
 function sync(){
 
-alert("SYNC BPM");
+alert("BPM SYNC READY");
 
 }
+
+
+
+
+
+// RCF MENU CONTROL
+
+
+function showPanel(panel){
+
+
+let panels=document.querySelectorAll(
+".panel"
+);
+
+
+panels.forEach(p=>{
+
+p.style.display="none";
+
+});
+
+
+
+let selected=document.getElementById(panel);
+
+
+if(selected){
+
+selected.style.display="block";
+
+}
+
+
+}
+
+
 
 
 
